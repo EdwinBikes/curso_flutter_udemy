765506de8dbdcfac54d6e62defc7e962dfa5f3a6
Edwin bikes <edwinbykes@hotmail.com>
2023-06-15 16:18:27 -0500

    cargue de la nueva app del curso

---
+++ clase_udemy_fernando_herrera/chat_flutter/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,64 @@
+import 'package:chat_flutter/presentation/widget/chat/her_message_bubble.dart';
+import 'package:chat_flutter/presentation/widget/chat/my_message_bubble.dart';
+import 'package:chat_flutter/presentation/widget/shared/message_field_box.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:chat_flutter/domain/entities/message.dart';
+
+import 'package:chat_flutter/presentation/providers/chat_provider.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://www.stylist.co.uk/images/app/uploads/2022/06/01105352/jennifer-aniston-crop-1654077521-1390x1390.jpg?w=256&h=256&fit=max&auto=format%2Ccompress'),
+          ),
+        ),
+        title: const Text('Mi amor ♥️'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                  controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messageList.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messageList[index];
+                       
+                      return (message.fromWho == FromWho.hers)
+                          ? HerMessageBubble( message: message )
+                          : MyMessageBubble( message: message );
+                    })),
+
+            /// Caja de texto de mensajes
+            MessageFieldBox(
+              // onValue: (value) => chatProvider.sendMessage(value),
+              onValue: chatProvider.sendMessage,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
